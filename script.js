4b5e97c50f44602c079d79be674d5fd93a068b3a
techikram <161257428+techikram@users.noreply.github.com>
2024-11-20 20:46:47 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const robot = document.querySelector(".robot-icon");
-  if (robot) {
-    robot.style.transform = "scale(0.5)";
-    setTimeout(() => {
-      robot.style.transition = "transform 0.5s";
-      robot.style.transform = "scale(1)";
-    }, 500);
-  }
-});
+
