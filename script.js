3da17fbec3436743d467f6d4405800cff1d2f923
techikram <161257428+techikram@users.noreply.github.com>
2024-11-20 20:00:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 
-console.log("¡Bienvenido a mi portafolio interactivo!");
+document.addEventListener("DOMContentLoaded", () => {
+  const title = document.querySelector(".animated-title");
+  title.style.transform = "translateX(-100%)";
+  title.style.transition = "transform 2s";
+  setTimeout(() => {
+    title.style.transform = "translateX(0)";
+  }, 500);
+});
