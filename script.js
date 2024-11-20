561f51b05b7da7d9bf0289283f7572d41714fc8a
techikram <161257428+techikram@users.noreply.github.com>
2024-11-20 22:19:23 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 
+function createParticles() {
+  const particlesContainer = document.querySelector('.particles');
+  const numberOfParticles = 30; 
+
+
+  for (let i = 0; i < numberOfParticles; i++) {
+    const particle = document.createElement('div');
+    particle.classList.add('particle');
+
+    particle.style.left = `${Math.random() * 100}%`;
+    particle.style.top = `${Math.random() * 100}%`; 
+
+
+    particlesContainer.appendChild(particle);
+  }
+}
+
+
+createParticles();
+
+
+document.addEventListener('scroll', () => {
+  const particles = document.querySelectorAll('.particle');
+  particles.forEach(particle => {
+    const offset = window.scrollY / 5; 
+    particle.style.transform = `translateY(${offset}px)`;
+  });
+});
+
