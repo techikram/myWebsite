a62cb11ca7086ef4939d50228148069c16eb074f
techikram <161257428+techikram@users.noreply.github.com>
2024-11-21 16:08:00 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const typingText = `Hi, I'm Ikram :) Welcome to my portfolio, where you'll find my recent works and learn a bit about me.`;
+  const typingElement = document.querySelector(".typing-effect");
+  let index = 0;
+
+  function type() {
+    if (index < typingText.length) {
+      typingElement.textContent += typingText.charAt(index);
+      index++;
+      setTimeout(type, 100); 
+    } else {
+      typingElement.style.borderRight = "none"; 
+    }
+  }
+
+  type();
+});
 
