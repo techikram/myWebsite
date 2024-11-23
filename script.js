f3b764ef474725ec9c3498037733b458e8293357
techikram <161257428+techikram@users.noreply.github.com>
2024-11-23 18:32:42 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 
+document.addEventListener("DOMContentLoaded", function () {
+  const text = "Hi, I'm Ikram :)";
+  const typingEffectElement = document.querySelector(".typing-effect");
+  let index = 0;
+
+  function typeText() {
+    if (index < text.length) {
+      typingEffectElement.textContent += text.charAt(index);
+      index++;
+      setTimeout(typeText, 150); 
+    }
+  }
+
+  typeText();
+});
