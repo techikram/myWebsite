012238d03f15b433d0550a82d28f5bc293042848
techikram <161257428+techikram@users.noreply.github.com>
2024-11-23 22:38:47 +0100

    Update script.js

--- script.js
+++ script.js
@@ -30,15 +30,28 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
-<script>
-  document.getElementById('show-more').addEventListener('click', function () {
-    const hiddenItems = document.querySelectorAll('.technologies-list .hidden');
-    hiddenItems.forEach((item) => {
-      item.style.display = item.style.display === 'none' || item.style.display === '' ? 'inline-block' : 'none';
-    });
-
+document.getElementById('show-more').addEventListener('click', function () {
+  const modal = document.getElementById('technologies-modal');
+  const button = this;
   
-    this.textContent = this.textContent === 'Show More' ? 'Show Less' : 'Show More';
+  // Abre el modal
+  modal.style.display = "block";
+  
+  // Cambiar texto del botón
+  button.textContent = 'Show Less';
+  
+  // Cerrar modal
+  document.querySelector('.close-btn').addEventListener('click', () => {
+    modal.style.display = "none";
+    button.textContent = 'Show More';
   });
-</script>
+  
+  // Cerrar modal al hacer clic fuera del contenido del modal
+  window.addEventListener('click', function(event) {
+    if (event.target === modal) {
+      modal.style.display = "none";
+      button.textContent = 'Show More';
+    }
+  });
+});
 
