095e68a876e7fba3b33fbbec9e1a8a0920513d62
techikram <161257428+techikram@users.noreply.github.com>
2024-11-23 18:45:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -14,3 +14,14 @@ document.addEventListener("DOMContentLoaded", function () {
 
   typeText();
 });
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  const hamburger = document.querySelector(".hamburger");
+  const navLinks = document.querySelector(".nav-links");
+
+  hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navLinks.classList.toggle("open");
+  });
+});
